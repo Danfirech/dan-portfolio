b78fa6589a3132ab493ba75b4a9f4349ddafac0b
Dan Possehl <danielbpossehl@gmail.com>
2021-12-25 07:04:10 -0500

    created left and right side of nav with containers

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  NavbarContainer,
+  Leftcontainer,
+  Rightcontainer,
+  NavbarInnerContainer,
+  NavbarExtendedContainer,
+  NavbarLinkContainer,
+} from "../styles/Navbar.style";
+import { Link } from "react-router-dom";
 
-const NavBar = () => {
-  return <div>NavBar</div>;
-};
+function NavBar() {
+  return (
+    <NavbarContainer>
+      <NavbarInnerContainer>
+        <Leftcontainer>
+          <NavbarLinkContainer>
+            <Link to="/">Home</Link>
+          </NavbarLinkContainer>
+        </Leftcontainer>
+        <Rightcontainer />
+      </NavbarInnerContainer>
+      <NavbarExtendedContainer></NavbarExtendedContainer>
+    </NavbarContainer>
+  );
+}
 
 export default NavBar;
--- src/styles/Navbar.style.js
+++ src/styles/Navbar.style.js
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 
-const NavBarContainer = styled.navbar`
+export const NavbarContainer = styled.nav`
   width: 100%;
   height: 80px;
   background-color: black;
   display: flex;
   flex-direction: column;
 `;
+
+export const Leftcontainer = styled.div`
+  flex: 70%;
+  display: flex;
+  align-items: center;
+  padding-left: 5%;
+`;
+
+export const Rightcontainer = styled.div`
+  flex: 30%;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 50px;
+`;
+
+export const NavbarInnerContainer = styled.div`
+  width: "100%";
+  height: 80px;
+  display: flex;
+`;
+
+export const NavbarLinkContainer = styled.div`
+  display: flex;
+`;
+
+export const NavbarExtendedContainer = styled.div``;
