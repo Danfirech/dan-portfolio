906a0b6819412a92a30cc5fa20780ad4af972a5f
Dan Possehl <danielbpossehl@gmail.com>
2021-12-25 06:46:05 -0500

    styles and components folder built

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import react from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
 
 function App() {
-  return <div>Dano</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/products" />
+        <Route path="/content" />
+        <Route path="/about" />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/styles/Navbar.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const NavBarContainer = styled.navbar`
+  width: 100%;
+  height: 80px;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+`;
