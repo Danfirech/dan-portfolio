9f452385eee72582ab32a8cb40bd597cca6a3f20
Dan Possehl <danielbpossehl@gmail.com>
2022-01-26 10:26:49 -0700

    title container

--- src/styles/ShowWork.Style.js
+++ src/styles/ShowWork.Style.js
@@ -92,7 +92,7 @@ export const Width2 = styled.div`
     weight: 100vw;
     padding-right: 0px;
     padding-left: 0px;
-    height: 1480px;
+    height: 1680px;
   }
   @media (max-width: 1000px) {
     flex-direction: column;
@@ -101,7 +101,7 @@ export const Width2 = styled.div`
     weight: 100vw;
     padding-right: 0px;
     padding-left: 0px;
-    height: 1480px;
+    height: 1680px;
   }
 `;
 
